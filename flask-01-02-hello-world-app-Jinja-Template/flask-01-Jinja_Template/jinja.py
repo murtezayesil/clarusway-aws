244c7e0d903fb9b606328f12bf74c4476ab961ed
murteza yesil <murtezayesil@protonmail.com>
2022-01-18 19:59:09 -0700

    2nd flask project

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-Jinja_Template/jinja.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    message = "Hello Clarusway"
+    return render_template("index.html", num1=34, num2=45, greeting = message, greet_size=3)
+
+if __name__=="__main__":
+    app.run(debug=True)
